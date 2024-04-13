08c4a2c5d44d2854c65016d35beb3fa1902661d8
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-04-14 00:25:45 +0300

    Closures Created.

---
+++ UniversitiesApp/Utilities/Closures/Closures.swift
@@ -0,0 +1,11 @@
+//
+//  Closures.swift
+//  UniversitiesApp
+//
+//  Created by Şehriban Yıldırım on 13.04.2024.
+//
+
+import Foundation
+
+public typealias VoidClosure  = (() -> Void)
+public typealias ErrorClosure = (Error) -> Void
