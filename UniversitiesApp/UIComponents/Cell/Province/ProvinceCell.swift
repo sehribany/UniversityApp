e83d1f5a775036f3c2b69caced979570cc8edb16
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-04-23 19:23:39 +0300

    ProvinceCell Created.

--- UniversitiesApp/UIComponents/Cell/Province/ProvinceCell.swift
+++ UniversitiesApp/UIComponents/Cell/Province/ProvinceCell.swift
@@ -44,6 +44,10 @@ class ProvinceCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
 }
 
 //MARK: -UILayout and Set
